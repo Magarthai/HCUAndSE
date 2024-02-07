ce048ef09f3dee2e4d29f099e32016f9e6f5520f
Rawisada <112146634+Rawisada@users.noreply.github.com>
2024-02-08 05:26:09 +0700

    Update ui edit request

--- client/src/components_hcu/AppointmentRequestManagementHistoryComponent.js
+++ client/src/components_hcu/AppointmentRequestManagementHistoryComponent.js
@@ -211,7 +211,7 @@ const AppointmentRequestManagementHistoryComponent = (props) => {
                 <p className="admin-textBody-large">Time : {showTime}</p>
             </div>
         </div>
-        <a href="/adminAppointmentRequestManagementComponent"><img src={arrow_icon} className="approval-icon admin-back-arrow"/></a>
+        <a href="/adminAppointmentRequestManagementComponent"><img src={arrow_icon} className="admin-back-arrow"/></a>
         <div className="admin">
             <div className="admin-header">
                 <p className="admin-hearder-item admin-textBody-large colorPrimary-800">รายการนัดหมาย</p>            
@@ -228,8 +228,8 @@ const AppointmentRequestManagementHistoryComponent = (props) => {
                                 <th className="admin-textBody-large colorPrimary-800" id="th_clinic">คลินิก</th>
                                 <th className="admin-textBody-large colorPrimary-800" id="th_dateOld">วันนัดหมายเดิม</th>
                                 <th className="admin-textBody-large colorPrimary-800" id="th_dateNew">วันนัดหมายที่ขอเปลี่ยน</th>
-                                <th className="admin-textBody-large colorPrimary-800" id="th_notation">เวลาเดิม</th>
-                                <th className="admin-textBody-large colorPrimary-800" id="th_notation">เวลาใหม่</th>
+                                <th className="admin-textBody-large colorPrimary-800" id="th_timeOld">เวลาเดิม</th>
+                                <th className="admin-textBody-large colorPrimary-800" id="th_timeNew">เวลาใหม่</th>
                                 <th className="admin-textBody-large colorPrimary-800" id="th_symptom">อาการ</th>
                                 <th className="admin-textBody-large colorPrimary-800" id="th_notation">หมายเหตุ</th>
                                 <th className="admin-textBody-large colorPrimary-800" id="th_approve">อนุมัติ</th>
