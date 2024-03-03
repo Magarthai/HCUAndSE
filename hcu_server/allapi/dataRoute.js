a602a25b2b4006e7f6933ea42dcb1445ad41d597
Magarthai <132775604+Magarthai@users.noreply.github.com>
2024-03-03 19:57:13 +0700

    Update dataRoute.js

--- hcu_server/allapi/dataRoute.js
+++ hcu_server/allapi/dataRoute.js
@@ -17,7 +17,7 @@ router.get('/data', async (req,res) => {
         dataSnapShot.forEach(doc => {
             data.push(doc.data());
         })
-        res.json(data);
+        res.send(data);
     } catch (error) {
         console.error(`Error fetching data: ${error}`);
         res.status(500).json({ error: 'Internal server error' });
