a1719aa21153e663091b36fb4cb665421097ab75
Magarthai <magargame@gmail.com>
2024-03-24 00:55:22 +0700

    information user backend

--- client/src/components_user/InformationAllComponent.js
+++ client/src/components_user/InformationAllComponent.js
@@ -3,9 +3,34 @@ import "../css/Login&SignupComponent.css";
 import "../css/UserInformationComponent.css";
 import NavbarUserComponent from './NavbarComponent';
 import HCU from "../picture/HCU.jpg"
-
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 const InformationAllComponent = (props) => {
+    const navigate = useNavigate();
+    const [information, setInformation] = useState([]);
+    const [check,setChcek] = useState(false);
+    useEffect(() => {
+        if(!check) {
+        fetchData();
+        setChcek(true);
+        }
+    })
 
+    const REACT_APP_MONGO_API = process.env.REACT_APP_MONGO_API
+    const fetchData = async() => {
+        console.log("XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+        const respone = await axios.get(`${REACT_APP_MONGO_API}/api/getAllInformation`)
+        if(respone.data) {
+            setInformation(respone.data)
+            console.log(respone.data,"dataaaaaaa");
+        }
+    }
+
+    const PreviewInformation = (informations) => {
+        if (informations) {
+        navigate('/information/preview', { state: { information: informations } });
+        }
+    }
 
     return (
         
@@ -21,22 +46,16 @@ const InformationAllComponent = (props) => {
             <div className="user-body">
                 <div className="user-body-infomation colorPrimary-800">
                     <h3>ข้อมูลทั่วไป</h3>
-                    <a href="/information/preview" className="user-card-infornation-flexbox">
+                    {information.map((information, index) => (
+                    <a onClick={() => PreviewInformation(information)}className="user-card-infornation-flexbox">
                         <div className="user-card-infornation-box-img">
-                            <img src={HCU}/>
+                            <img src={information.image}/>
                         </div>
                         <div className="user-card-infornation-box-name">
-                            <p>รถฉุกเฉินในมจธ.</p>
+                            <p>{information.informationName}</p>
                         </div>
                     </a>
-                    <a href="/information/preview" className="user-card-infornation-flexbox">
-                        <div className="user-card-infornation-box-img">
-                            <img src={HCU}/>
-                        </div>
-                        <div className="user-card-infornation-box-name">
-                            <p>รถฉุกเฉินในมจธ.</p>
-                        </div>
-                    </a>
+                    ))}
                 </div>
                 
                 
--- client/src/components_user/InformationPreviewComponent.js
+++ client/src/components_user/InformationPreviewComponent.js
@@ -3,10 +3,30 @@ import "../css/Login&SignupComponent.css";
 import "../css/UserInformationComponent.css";
 import NavbarUserComponent from './NavbarComponent';
 import information from "../picture/information.jpg"
-
+import Swal from "sweetalert2";
+import { useNavigate, useLocation } from "react-router-dom";
 const InformationPreviewComponent = (props) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { information } = location.state || {};
+    useEffect(() => {
+        document.title = 'Health Care Unit';
+        if (!information) {
+            Swal.fire({
+                icon: 'error',
+                title: 'เกิดข้อผิดพลาด',
+                text: 'ไม่มีข้อมูลกิจกรรม',
+                confirmButtonColor: '#263A50',
+                customClass: {
 
-
+                    confirmButton: 'custom-confirm-button',
+                }
+            }).then(() => {
+                navigate('/adminInformationAll');
+            });
+        }
+    
+    }); 
     return (
         
         <div className="user">
@@ -20,12 +40,12 @@ const InformationPreviewComponent = (props) => {
             </header>
             <div className="user-body">
                 <div className="user-body-infomation-preview colorPrimary-800">
-                    <h3>รถฉุกเฉินในมจธ.</h3>
+                    {information &&<h3>{information.informationName}.</h3>}
                     <div className="center">
-                        <img src={information} className="user-information-preview-img"/>
+                    {information &&<img src={information.image} className="user-information-preview-img"/>}
                     </div>
                    
-                    <p className="textBody-large">dsgdddddddddddddddasfaaaffsgdddddddddddddddasfaaaff</p>
+                    {information &&<p className="textBody-large">{information.informationDetail}</p>}
                     
                 </div>
             </div>
