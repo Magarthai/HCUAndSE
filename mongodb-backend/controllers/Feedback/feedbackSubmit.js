88a25770556e34fcabd4f637eeb1fcb87676b141
Magarthai <132775604+Magarthai@users.noreply.github.com>
2024-04-26 03:35:55 +0700

    Update feedbackSubmit.js

--- mongodb-backend/controllers/Feedback/feedbackSubmit.js
+++ mongodb-backend/controllers/Feedback/feedbackSubmit.js
@@ -1,7 +1,7 @@
 const router = require('express').Router(); 
 const Feedback = require("../../models/feedback.model");
 const asyncHandler = require('express-async-handler');
-
+const moment = require('moment-timezone');
 router.post('/createFeedback', asyncHandler(async (req, res) => {
     try {
 
