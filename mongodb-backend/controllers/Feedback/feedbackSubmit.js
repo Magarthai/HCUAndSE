a49543349d6bb53df96e46f59db864939045f5b1
Magarthai <132775604+Magarthai@users.noreply.github.com>
2024-04-26 02:36:31 +0700

    Update feedbackSubmit.js

--- mongodb-backend/controllers/Feedback/feedbackSubmit.js
+++ mongodb-backend/controllers/Feedback/feedbackSubmit.js
@@ -1,10 +1,18 @@
 const router = require('express').Router(); 
 const Feedback = require("../../models/feedback.model");
 const asyncHandler = require('express-async-handler');
-
+const moment = require('moment-timezone');
 router.post('/createFeedback', asyncHandler(async (req, res) => {
+    const date = moment(req.body.date).tz('Asia/Bangkok')
     try{
-    const newFeedback = await Feedback.create(req.body);
+    const data = {
+        score: req.body.score,
+        detail: req.body.detail,
+        clinic: req.body.clinic,
+        typeFeedback: req.body.typeFeedback,
+        date: date
+    }
+    const newFeedback = await data.create(req.body);
     res.json("success")
     } catch (error) {
         console.log(error, "createFeedback");
