42334f9abc969206fddae778e78585cf8b384173
Magarthai <132775604+Magarthai@users.noreply.github.com>
2024-04-26 03:17:14 +0700

    Update feedbackSubmit.js

--- mongodb-backend/controllers/Feedback/feedbackSubmit.js
+++ mongodb-backend/controllers/Feedback/feedbackSubmit.js
@@ -6,7 +6,8 @@ router.post('/createFeedback', asyncHandler(async (req, res) => {
     try{
         if (req.body.date) {
         const bangkokDate = moment.tz(req.body.date, 'Asia/Bangkok'); 
-        req.body.date = bangkokDate.toISOString(); 
+        req.body.date = bangkokDate
+        req.body.date = req.body.date.hour(17).minute(0).second(0).millisecond(0);
         }
     const newFeedback = await Feedback.create(req.body);
     res.json("success")
