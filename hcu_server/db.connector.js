257f8b5c39cde5f55bf1c000f52354aff50ae7dd
Magarthai <132775604+Magarthai@users.noreply.github.com>
2024-05-28 00:05:48 +0700

    test

--- hcu_server/db.connector.js
+++ hcu_server/db.connector.js
@@ -1,7 +1,7 @@
 const Mongoose = require("mongoose")
 
 const dbConnect = () => {
-    const connect = Mongoose.connect("mongodb://root:password@pumipat.trueddns.com:63711")
+    const connect = Mongoose.connect("mongodb://root:password@pumipat.trueddns.com:63711/mongodb")
     console.log("Database is connected successfully!");
 }
 
